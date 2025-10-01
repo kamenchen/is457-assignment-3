3cb436909d971deaeffe800c445548cd8e8ccd8f
Kamen Chen Yu <Kamen.ChenYu01@student.csulb.edu>
2025-10-01 14:04:22 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'is457-assignment-3',
+  webDir: 'www'
+};
+
+export default config;
