69a8d2fbcc35daae529b05238ce288807a5269c6
Kamen Chen Yu <Kamen.ChenYu01@student.csulb.edu>
2025-10-01 18:19:19 -0700

    change 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import quotes from 'src/assets/data/quotes';
+import { Quotes } from 'src/assets/data/quotes.interface';
 
 @Component({
   selector: 'app-tab2',
@@ -6,8 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  quoteCollection: {category: string, quotes: Quotes[]}[];
+  constructor(
+    private alertController: AlertController
+  ) {}
 
-  constructor() {}
-
+  ngOnInit(){
+    this.quoteCollection = quotes;
+    console.log(this.quoteCollection);
+  }
+  async onAddToFavorite(SelectedQuote: Quotes) {
+    const alert = await this.alertController.create({
+      header: 'Add Quote',
+      subHeader: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {text: 'Confirm',},
+        {text: 'Not Confirm',}
+      ]
+    });
+    await alert.present();
+  }
 }
